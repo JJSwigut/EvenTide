64bc86c1927f98da83d98e934ff4838aff69b37b
Swiggatron <jjswigut@gmail.com>
2020-10-28 13:29:12 -0400

    Maps

--- app/src/main/java/com/jjswigut/eventide/data/remote/Service.kt
+++ app/src/main/java/com/jjswigut/eventide/data/remote/Service.kt
@@ -6,9 +6,9 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface Service {
-    @GET("?stations&lat=33.768321&lon=-118.195617&stationDistance=50&key=05b6e10b-e0cc-4a27-b983-29b858395a35")
+    @GET("v2?stations&lat=33.768321&lon=-118.195617&stationDistance=50&key=05b6e10b-e0cc-4a27-b983-29b858395a35")
     suspend fun getStations(): Response<StationList>
 
-    @GET("?heights&extremes&date=2020-10-18&lat=33.768321&lon=-118.195617&days=7&key=05b6e10b-e0cc-4a27-b983-29b858395a35")
+    @GET("v2?heights&extremes&date=2020-10-18&lat=33.768321&lon=-118.195617&days=7&key=05b6e10b-e0cc-4a27-b983-29b858395a35")
     suspend fun getTides(): Response<TideList>
 }
--- app/src/main/java/com/jjswigut/eventide/ui/map/MapsFragment.kt
+++ app/src/main/java/com/jjswigut/eventide/ui/map/MapsFragment.kt
@@ -1,28 +1,26 @@
 package com.jjswigut.eventide.ui.map
 
-import android.Manifest
-import android.app.Activity
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.app.ActivityCompat
+import androidx.fragment.app.viewModels
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.jjswigut.eventide.R
 import com.jjswigut.eventide.ui.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MapsFragment : BaseFragment() {
 
-    private val REQUEST_LOCATION_PERMISSION = 1
+    private val viewModel: MapFragmentViewModel by viewModels()
 
     private val callback = OnMapReadyCallback { googleMap ->
-        enableMyLocation(googleMap)
+        viewModel.enableMyLocation(googleMap, requireContext())
         val sydney = LatLng(-34.0, 151.0)
         googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
@@ -42,23 +40,5 @@ class MapsFragment : BaseFragment() {
         mapFragment?.getMapAsync(callback)
     }
 
-    private fun enableMyLocation(map: GoogleMap) {
-        if (ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                context as Activity,
-                arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
-                REQUEST_LOCATION_PERMISSION
-            )
-            return
-        }
-        map.isMyLocationEnabled = true
-    }
 
 }
