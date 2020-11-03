bd2df2892dd572c3b4b5eb188fffb3e25ef06d99
Swiggatron <jjswigut@gmail.com>
2020-11-03 12:50:51 -0500

    Retrofitting

--- app/src/main/java/com/jjswigut/eventide/data/remote/RemoteDataSource.kt
+++ app/src/main/java/com/jjswigut/eventide/data/remote/RemoteDataSource.kt
@@ -1,11 +1,12 @@
 package com.jjswigut.eventide.data.remote
 
+import com.jjswigut.eventide.R
 import javax.inject.Inject
 
 class RemoteDataSource @Inject constructor(
     private val service: Service
 ): BaseDataSource() {
 
-    suspend fun getStations() = getResult { service.getStations() }
-    suspend fun getTides() = getResult { service.getTides() }
+    suspend fun getStations() = getResult { service.getStations(R.string.tides_api_key.toString()) }
+    suspend fun getTides() = getResult { service.getTides(R.string.tides_api_key.toString()) }
 }
--- app/src/main/java/com/jjswigut/eventide/data/remote/Service.kt
+++ app/src/main/java/com/jjswigut/eventide/data/remote/Service.kt
@@ -1,14 +1,23 @@
 package com.jjswigut.eventide.data.remote
 
+
 import com.jjswigut.eventide.data.entities.StationList
 import com.jjswigut.eventide.data.entities.TideList
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
 
 interface Service {
-    @GET("v2?stations&lat=33.768321&lon=-118.195617&stationDistance=50&key=05b6e10b-e0cc-4a27-b983-29b858395a35")
-    suspend fun getStations(): Response<StationList>
 
-    @GET("v2?heights&extremes&date=2020-10-18&lat=33.768321&lon=-118.195617&days=7&key=05b6e10b-e0cc-4a27-b983-29b858395a35")
-    suspend fun getTides(): Response<TideList>
+    @GET("v2?stations&lat=33.768321&lon=-118.195617&stationDistance=50&key={api_key}")
+    suspend fun getStations(
+        @Path("api_key") tideApiKey: String
+    ): Response<StationList>
+
+    @GET("v2?heights&extremes&date=2020-10-18&lat=33.768321&lon=-118.195617&days=7&key={api_key}")
+    suspend fun getTides(
+        @Query("api_key") tideApiKey: String
+    ): Response<TideList>
 }
