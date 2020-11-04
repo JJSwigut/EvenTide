e2f0741c534cd201f9fb90d477e3aa254eac459a
Swiggatron <jjswigut@gmail.com>
2020-11-04 18:43:27 -0500

    Retrofitting

--- app/src/main/java/com/jjswigut/eventide/data/remote/Service.kt
+++ app/src/main/java/com/jjswigut/eventide/data/remote/Service.kt
@@ -4,11 +4,16 @@ import com.jjswigut.eventide.data.entities.StationList
 import com.jjswigut.eventide.data.entities.TideList
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface Service {
-    @GET("v2?stations&lat=33.768321&lon=-118.195617&stationDistance=50&key=05b6e10b-e0cc-4a27-b983-29b858395a35")
-    suspend fun getStations(): Response<StationList>
+    @GET("v2?stations&lat=33.768321&lon=-118.195617&stationDistance=50&key={api_key}")
+    suspend fun getStations(
+        @Path("api_key") tideApikey: String
+    ): Response<StationList>
 
-    @GET("v2?heights&extremes&date=2020-10-18&lat=33.768321&lon=-118.195617&days=7&key=05b6e10b-e0cc-4a27-b983-29b858395a35")
-    suspend fun getTides(): Response<TideList>
+    @GET("v2?heights&extremes&date=2020-10-18&lat=33.768321&lon=-118.195617&days={api_key}")
+    suspend fun getTides(
+        @Path("api_key") tideApikey: String
+    ): Response<TideList>
 }
