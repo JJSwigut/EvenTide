76c6d466ca2009da5bb63ce18282de6fe2d9b258
Swiggatron <jjswigut@gmail.com>
2020-12-11 13:34:32 -0500

    settings fragment with no menu

--- app/src/main/java/com/jjswigut/eventide/ui/SettingsFragment.kt
+++ app/src/main/java/com/jjswigut/eventide/ui/SettingsFragment.kt
@@ -1,15 +1,23 @@
 package com.jjswigut.eventide.ui
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.preference.PreferenceFragmentCompat
 import com.jjswigut.eventide.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
-
     }
 
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        super.onPrepareOptionsMenu(menu)
+        menu.clear()
+
+    }
 }
